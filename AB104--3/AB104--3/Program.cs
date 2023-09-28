0893198e70bf75137dd9a86767b51080f1adc552
tofiq7770 <tofigtn@code.edu.az>
2023-09-28 18:08:15 +0400

    Adding Files

---
+++ AB104--3/AB104--3/Program.cs
@@ -0,0 +1,44 @@
+﻿
+//task1
+
+//int[] arr = { 1, 3, 5, 7 };
+//int sum = 0;
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    sum+=arr[i];
+//}
+//Console.WriteLine(sum);
+
+//task2
+
+//string[] arr = { "1234", "ttaatt", "tart", "abc", "4321" };
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i].Length == 4) 
+//    {
+//        Console.WriteLine(arr[i]);
+//    }
+//}
+
+//task3
+
+//int[] arr = { 1, 3, 44,5, 7, 9 };
+
+//int length = arr.Length;
+
+//for (int i = 0; i < length-1; i++)
+//{
+//    if (arr[i]<arr[i+1])
+//    {
+//        int temp = arr[i];
+//        arr[i] = arr[i+1];
+//        arr[i+1] = temp;
+//        i = -1;
+//    }
+//}
+//foreach (var item in arr)
+//{
+//    Console.WriteLine(item);
+//}
